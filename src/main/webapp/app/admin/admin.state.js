c6f41f849244f706207fe041693cd69b1a9375ac
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 21:02:27 +0530

    initial base service using generator v4.4.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('spotGarageApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
