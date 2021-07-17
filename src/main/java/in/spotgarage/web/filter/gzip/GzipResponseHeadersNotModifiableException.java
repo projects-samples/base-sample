3b26466c154a416cdb023bf7f527d90bcb9e3d86
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:30:24 +0530

    initial base service using generator v1.9.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/web/filter/gzip/GzipResponseHeadersNotModifiableException.java
@@ -0,0 +1,10 @@
+package in.spotgarage.web.filter.gzip;
+
+import javax.servlet.ServletException;
+
+public class GzipResponseHeadersNotModifiableException extends ServletException {
+
+    public GzipResponseHeadersNotModifiableException(String message) {
+        super(message);
+    }
+}
