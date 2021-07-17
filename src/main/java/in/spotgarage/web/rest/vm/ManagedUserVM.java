ffedd348c35be9b588ea63cd836d748b78b5b9fc
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 18:23:44 +0530

    initial base service using generator v4.14.3
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,20 @@
+package in.spotgarage.web.rest.vm;
+
+import in.spotgarage.service.dto.UserDTO;
+import javax.validation.constraints.Size;
+
+/**
+ * View Model extending the UserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends UserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" +
+            "} " + super.toString();
+    }
+}
