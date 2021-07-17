9571e43bed21e849d0acbfd671c48404efe494dc
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 18:29:14 +0530

    initial base service using generator v4.14.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+
+import { WindowRef } from './tracker/window.service';
+import {
+    SpotGarageSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        SpotGarageSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        FindLanguageFromKeyPipe,
+        JhiLanguageHelper,
+        WindowRef,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'en'
+        },
+    ],
+    exports: [
+        SpotGarageSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class SpotGarageSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,56 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    SpotGarageSharedLibsModule,
+    SpotGarageSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+} from './';
+
+@NgModule({
+    imports: [
+        SpotGarageSharedLibsModule,
+        SpotGarageSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        SpotGarageSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class SpotGarageSharedModule {}
