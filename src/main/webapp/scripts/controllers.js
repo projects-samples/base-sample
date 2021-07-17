bb13fe5d1f594a56421fab8ddd0227bf87112256
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:56:20 +0530

    initial base service using generator v0.9.3
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/scripts/controllers.js
@@ -0,0 +1,149 @@
+'use strict';
+
+/* Controllers */
+
+SpotGarageApp.controller('MainController', ['$scope',
+    function ($scope) {
+    }]);
+
+SpotGarageApp.controller('AdminController', ['$scope',
+    function ($scope) {
+    }]);
+
+SpotGarageApp.controller('LanguageController', ['$scope', '$translate',
+    function ($scope, $translate) {
+        $scope.changeLanguage = function (languageKey) {
+            $translate.use(languageKey);
+        };
+    }]);
+
+SpotGarageApp.controller('MenuController', ['$scope',
+    function ($scope) {
+    }]);
+
+SpotGarageApp.controller('LoginController', ['$scope', '$location', 'AuthenticationSharedService',
+    function ($scope, $location, AuthenticationSharedService) {
+        $scope.rememberMe = true;
+        $scope.login = function () {
+            AuthenticationSharedService.login({
+                username: $scope.username,
+                password: $scope.password,
+                rememberMe: $scope.rememberMe,
+                success: function () {
+                    $location.path('');
+                }
+            })
+        }
+    }]);
+
+SpotGarageApp.controller('LogoutController', ['$location', 'AuthenticationSharedService',
+    function ($location, AuthenticationSharedService) {
+        AuthenticationSharedService.logout({
+            success: function () {
+                $location.path('');
+            }
+        });
+    }]);
+
+SpotGarageApp.controller('SettingsController', ['$scope', 'resolvedAccount', 'Account',
+    function ($scope, resolvedAccount, Account) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.settingsAccount = resolvedAccount;
+
+        $scope.save = function () {
+            Account.save($scope.settingsAccount,
+                function (value, responseHeaders) {
+                    $scope.error = null;
+                    $scope.success = 'OK';
+                    $scope.settingsAccount = Account.get();
+                },
+                function (httpResponse) {
+                    $scope.success = null;
+                    $scope.error = "ERROR";
+                });
+        };
+    }]);
+
+SpotGarageApp.controller('PasswordController', ['$scope', 'Password',
+    function ($scope, Password) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.doNotMatch = null;
+        $scope.changePassword = function () {
+            if ($scope.password != $scope.confirmPassword) {
+                $scope.doNotMatch = "ERROR";
+            } else {
+                $scope.doNotMatch = null;
+                Password.save($scope.password,
+                    function (value, responseHeaders) {
+                        $scope.error = null;
+                        $scope.success = 'OK';
+                    },
+                    function (httpResponse) {
+                        $scope.success = null;
+                        $scope.error = "ERROR";
+                    });
+            }
+        };
+    }]);
+
+SpotGarageApp.controller('SessionsController', ['$scope', 'resolvedSessions', 'Sessions',
+    function ($scope, resolvedSessions, Sessions) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.sessions = resolvedSessions;
+        $scope.invalidate = function (series) {
+            Sessions.delete({series: encodeURIComponent(series)},
+                function (value, responseHeaders) {
+                    $scope.error = null;
+                    $scope.success = "OK";
+                    $scope.sessions = Sessions.get();
+                },
+                function (httpResponse) {
+                    $scope.success = null;
+                    $scope.error = "ERROR";
+                });
+        };
+    }]);
+
+ SpotGarageApp.controller('MetricsController', ['$scope', 'resolvedMetrics', 'HealthCheckService',
+    function ($scope, resolvedMetrics, HealthCheckService) {
+        $scope.metrics = resolvedMetrics;
+        HealthCheckService.check();
+
+
+        resolvedMetrics.$get({}, function(items) {
+            $scope.servicesStats = {};
+            $scope.cachesStats = {};
+            angular.forEach(items.timers, function(value, key) {
+                if (key.indexOf("web.rest") != -1) {
+                    $scope.servicesStats[key] = value;
+                }
+
+                if (key.indexOf("net.sf.ehcache.Cache") != -1) {
+                    // remove gets or puts
+                    var index = key.lastIndexOf(".");
+                    var newKey = key.substr(0, index);
+
+                    // Keep the name of the domain
+                    index = newKey.lastIndexOf(".");
+                    $scope.cachesStats[newKey] = {
+                        'name': newKey.substr(index + 1),
+                        'value': value
+                    };
+                }
+            });
+        });
+    }]);
+
+SpotGarageApp.controller('LogsController', ['$scope', 'resolvedLogs', 'LogsService',
+    function ($scope, resolvedLogs, LogsService) {
+        $scope.loggers = resolvedLogs;
+
+        $scope.changeLevel = function (name, level) {
+            LogsService.changeLevel({name: name, level: level}, function () {
+                $scope.loggers = LogsService.findAll();
+            });
+        }
+    }]);
