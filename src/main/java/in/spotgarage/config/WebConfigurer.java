94f3254c4922444e36b0f65eacc9d4faec12353c
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 01:04:41 +0530

    initial base service using generator v0.8.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/config/WebConfigurer.java
@@ -0,0 +1,175 @@
+package in.spotgarage.config;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.health.HealthCheckRegistry;
+import com.codahale.metrics.servlet.InstrumentedFilter;
+import com.codahale.metrics.servlets.MetricsServlet;
+import in.spotgarage.web.filter.CachingHttpHeadersFilter;
+import in.spotgarage.web.filter.StaticResourcesProductionFilter;
+import in.spotgarage.web.filter.gzip.GZipServletFilter;
+import in.spotgarage.web.servlet.HealthCheckServlet;
+import org.atmosphere.cache.UUIDBroadcasterCache;
+import org.atmosphere.cpr.AtmosphereServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.inject.Inject;
+import javax.servlet.*;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Configuration of web application with Servlet 3.0 APIs.
+ */
+@Configuration
+@AutoConfigureAfter(CacheConfiguration.class)
+public class WebConfigurer implements ServletContextInitializer {
+
+    private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
+
+    @Inject
+    private MetricRegistry metricRegistry;
+
+    @Inject
+    private HealthCheckRegistry healthCheckRegistry;
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("Web application configuration");
+        EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+        
+        initMetrics(servletContext, disps);
+        initAtmosphereServlet(servletContext);
+        if (WebApplicationContextUtils
+                .getRequiredWebApplicationContext(servletContext)
+                .getBean(Environment.class)
+                .acceptsProfiles(Constants.SPRING_PROFILE_PRODUCTION)) {
+
+            initStaticResourcesProductionFilter(servletContext, disps);
+            initCachingHttpHeadersFilter(servletContext, disps);
+        }
+        initGzipFilter(servletContext, disps);
+
+        log.debug("Web application fully configured");
+    }
+
+    /**
+     * Initializes the GZip filter.
+     */
+    private void initGzipFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
+        log.debug("Registering GZip Filter");
+
+        FilterRegistration.Dynamic compressingFilter = servletContext.addFilter("gzipFilter", new GZipServletFilter());
+        Map<String, String> parameters = new HashMap<String, String>();
+
+        compressingFilter.setInitParameters(parameters);
+
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.css");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.json");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.html");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "*.js");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "/app/rest/*");
+        compressingFilter.addMappingForUrlPatterns(disps, true, "/metrics/*");
+
+        compressingFilter.setAsyncSupported(true);
+    }
+
+    /**
+     * Initializes the static resources production Filter.
+     */
+    private void initStaticResourcesProductionFilter(ServletContext servletContext,
+                                                     EnumSet<DispatcherType> disps) {
+
+        log.debug("Registering static resources production Filter");
+        FilterRegistration.Dynamic staticResourcesProductionFilter =
+                servletContext.addFilter("staticResourcesProductionFilter",
+                        new StaticResourcesProductionFilter());
+
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/index.html");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/images/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/fonts/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/styles/*");
+        staticResourcesProductionFilter.addMappingForUrlPatterns(disps, true, "/views/*");
+        staticResourcesProductionFilter.setAsyncSupported(true);
+    }
+
+    /**
+     * Initializes the cachig HTTP Headers Filter.
+     */
+    private void initCachingHttpHeadersFilter(ServletContext servletContext,
+                                              EnumSet<DispatcherType> disps) {
+        log.debug("Registering Cachig HTTP Headers Filter");
+        FilterRegistration.Dynamic cachingHttpHeadersFilter =
+                servletContext.addFilter("cachingHttpHeadersFilter",
+                        new CachingHttpHeadersFilter());
+
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/images/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/fonts/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
+        cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/styles/*");
+        cachingHttpHeadersFilter.setAsyncSupported(true);
+    }
+
+    /**
+     * Initializes Metrics.
+     */
+    private void initMetrics(ServletContext servletContext, EnumSet<DispatcherType> disps) {
+        log.debug("Initializing Metrics registries");
+        servletContext.setAttribute(InstrumentedFilter.REGISTRY_ATTRIBUTE,
+                metricRegistry);
+        servletContext.setAttribute(MetricsServlet.METRICS_REGISTRY,
+                metricRegistry);
+        servletContext.setAttribute(HealthCheckServlet.HEALTH_CHECK_REGISTRY,
+                healthCheckRegistry);
+
+        log.debug("Registering Metrics Filter");
+        FilterRegistration.Dynamic metricsFilter = servletContext.addFilter("webappMetricsFilter",
+                new InstrumentedFilter());
+
+        metricsFilter.addMappingForUrlPatterns(disps, true, "/*");
+        metricsFilter.setAsyncSupported(true);
+
+        log.debug("Registering Metrics Servlet");
+        ServletRegistration.Dynamic metricsAdminServlet =
+                servletContext.addServlet("metricsServlet", new MetricsServlet());
+
+        metricsAdminServlet.addMapping("/metrics/metrics/*");
+        metricsAdminServlet.setAsyncSupported(true);
+        metricsAdminServlet.setLoadOnStartup(2);
+
+        log.debug("Registering HealthCheck Servlet");
+        ServletRegistration.Dynamic healthCheckServlet =
+                servletContext.addServlet("healthCheckServlet", new HealthCheckServlet());
+
+        healthCheckServlet.addMapping("/metrics/healthcheck/*");
+        healthCheckServlet.setAsyncSupported(true);
+        healthCheckServlet.setLoadOnStartup(2);
+    }
+
+    /**
+     * Initializes Atmosphere.
+     */
+    private void initAtmosphereServlet(ServletContext servletContext) {
+        log.debug("Registering Atmosphere Servlet");
+        ServletRegistration.Dynamic atmosphereServlet =
+                servletContext.addServlet("atmosphereServlet", new AtmosphereServlet());
+
+        atmosphereServlet.setInitParameter("org.atmosphere.cpr.packages", "com.mycompany.myapp.web.websocket");
+        atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcasterCacheClass", UUIDBroadcasterCache.class.getName());
+        atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.shareableThreadPool", "true");
+        atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.maxProcessingThreads", "10");
+        atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.maxAsyncWriteThreads", "10");
+
+        atmosphereServlet.addMapping("/websocket/*");
+        atmosphereServlet.setLoadOnStartup(3);
+        atmosphereServlet.setAsyncSupported(true);
+    }
+}
