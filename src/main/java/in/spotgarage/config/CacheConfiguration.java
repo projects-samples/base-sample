60a5b1db9209ce353c54881a6d8065380301c552
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:46:24 +0530

    initial base service using generator v0.17.1
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/config/CacheConfiguration.java
@@ -0,0 +1,100 @@
+package in.spotgarage.config;
+
+import com.codahale.metrics.MetricRegistry;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.instance.HazelcastInstanceFactory;
+import com.hazelcast.config.MapConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import java.util.SortedSet;
+
+@Configuration
+@EnableCaching
+@AutoConfigureAfter(value = {MetricsConfiguration.class, DatabaseConfiguration.class})
+public class CacheConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);  
+
+    private static HazelcastInstance hazelcastInstance;
+
+    @Inject
+    private Environment env;
+
+    @Inject
+    private MetricRegistry metricRegistry;
+
+    private net.sf.ehcache.CacheManager cacheManager;
+
+    @PreDestroy
+    public void destroy() {
+        log.info("Remove Cache Manager metrics");
+        SortedSet<String> names = metricRegistry.getNames();
+        for (String name : names) {
+            metricRegistry.remove(name);
+        }
+        log.info("Closing Cache Manager");
+        cacheManager.shutdown();
+        Hazelcast.shutdownAll();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        log.debug("Starting Ehcache");
+        cacheManager = net.sf.ehcache.CacheManager.create();
+        cacheManager.getConfiguration().setMaxBytesLocalHeap(env.getProperty("cache.ehcache.maxBytesLocalHeap", String.class, "16M"));
+        log.debug("Registring Ehcache Metrics gauges");
+        EhCacheCacheManager ehCacheManager = new EhCacheCacheManager();
+        ehCacheManager.setCacheManager(cacheManager);
+        return ehCacheManager;
+    }
+
+    @PostConstruct
+    private HazelcastInstance hazelcastInstance() {
+        final Config config = new Config();
+        config.setInstanceName("SpotGarage");
+        config.getNetworkConfig().setPort(5701);
+        config.getNetworkConfig().setPortAutoIncrement(true);
+
+        if (env.acceptsProfiles(Constants.SPRING_PROFILE_DEVELOPMENT)) {
+          System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
+
+          config.getNetworkConfig().getJoin().getAwsConfig().setEnabled(false);
+          config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+          config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
+        }
+        
+        config.getMapConfigs().put("my-sessions", initializeClusteredSession());
+
+        hazelcastInstance = HazelcastInstanceFactory.newHazelcastInstance(config);
+
+        return hazelcastInstance;
+    }
+
+    private MapConfig initializeClusteredSession() {
+        MapConfig mapConfig = new MapConfig();
+
+        mapConfig.setBackupCount(env.getProperty("cache.hazelcast.backupCount", Integer.class, 1));
+        mapConfig.setTimeToLiveSeconds(env.getProperty("cache.timeToLiveSeconds", Integer.class, 3600));
+        return mapConfig;
+    }
+    
+    /**
+    * @return the unique instance.
+    */
+    public static HazelcastInstance getHazelcastInstance() {
+        return hazelcastInstance;
+    }
+}
---
+++ src/main/java/in/spotgarage/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package in.spotgarage.repository;
+
+import in.spotgarage.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
