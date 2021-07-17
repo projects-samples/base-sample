88ec7a60220d60c4a19b3a313b06a422dbc71717
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:07:04 +0530

    initial base service using generator v2.16.1
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/scripts/components/form/pagination.directive.js
@@ -0,0 +1,9 @@
+/* globals $ */
+'use strict';
+
+angular.module('spotgarageApp')
+    .directive('spotgarageAppPagination', function() {
+        return {
+            templateUrl: 'scripts/components/form/pagination.html'
+        };
+    });
