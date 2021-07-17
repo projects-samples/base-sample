95fcee9d43481472c2bfdfa693a490ab6e1916b9
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 14:39:49 +0530

    initial base service using generator v6.10.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package in.spotgarage.repository;
+
+import in.spotgarage.domain.PersistentToken;
+import in.spotgarage.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data MongoDB repository for the {@link PersistentToken} entity.
+ */
+public interface PersistentTokenRepository extends MongoRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
