a269f5e5853a53740e452c5e4b5af9d41cf5768a
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:27:38 +0530

    initial base service using generator v1.10.2
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/web/websocket/TrackerService.java
@@ -0,0 +1,9 @@
+package in.spotgarage.web.websocket;
+
+import org.atmosphere.config.service.ManagedService;
+
+@ManagedService(
+        path = "/websocket/tracker")
+public class TrackerService {
+
+}
