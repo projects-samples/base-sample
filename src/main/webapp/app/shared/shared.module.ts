38dda4a3f68ddcfd8929586210ce74d4a1b8cd78
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 20:38:16 +0530

    initial base service using generator v4.8.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,56 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    SpotGarageSharedLibsModule,
+    SpotGarageSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        SpotGarageSharedLibsModule,
+        SpotGarageSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        SpotGarageSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class SpotGarageSharedModule {}
