f37343b0ad2db74d73e64b990687a4de8e0a72bd
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 14:05:22 +0530

    initial base service using generator v7.0.0-beta.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/app/core/user/user.model.ts
@@ -0,0 +1,42 @@
+export interface IUser {
+  id?: number;
+  login?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  activated?: boolean;
+  langKey?: string;
+  authorities?: string[];
+  createdBy?: string;
+  createdDate?: Date;
+  lastModifiedBy?: string;
+  lastModifiedDate?: Date;
+  password?: string;
+}
+
+export class User implements IUser {
+  constructor(
+    public id?: number,
+    public login?: string,
+    public firstName?: string,
+    public lastName?: string,
+    public email?: string,
+    public activated?: boolean,
+    public langKey?: string,
+    public authorities?: string[],
+    public createdBy?: string,
+    public createdDate?: Date,
+    public lastModifiedBy?: string,
+    public lastModifiedDate?: Date,
+    public password?: string
+  ) {}
+}
+
+export interface IPublicUser {
+  id?: number;
+  name?: string;
+}
+
+export class PublicUser implements IPublicUser {
+  constructor(public id?: number, public login?: string) {}
+}
