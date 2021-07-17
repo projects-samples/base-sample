d71da9fb303dda016ba3c41b82ef833a0059adc5
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:37:55 +0530

    initial base service using generator v1.3.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/config/hazelcast/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Hazelcast configuration.
+ */
+package in.spotgarage.config.hazelcast;
---
+++ src/main/java/in/spotgarage/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using the Atmosphere framework.
+ */
+package in.spotgarage.web.websocket;
