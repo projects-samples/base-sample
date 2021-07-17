4ea6a9306f3ed8a03e7755262a537368c1ae39ba
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 14:00:05 +0530

    initial base service using generator v7.0.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/test/java/in/spotgarage/IntegrationTest.java
@@ -0,0 +1,17 @@
+package in.spotgarage;
+
+import in.spotgarage.SpotGarageApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = SpotGarageApp.class)
+public @interface IntegrationTest {
+}
