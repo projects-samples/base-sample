5aa4c68e5bdbb41e825a1377229e8d32f0e1e767
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 14:47:37 +0530

    initial base service using generator v6.9.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/repository/search/UserSearchRepository.java
@@ -0,0 +1,11 @@
+package in.spotgarage.repository.search;
+
+import in.spotgarage.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
