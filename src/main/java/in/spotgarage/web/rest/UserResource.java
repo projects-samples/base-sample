2cc310a1c807d4b89a772a683c557fbfc30d789a
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:49:30 +0530

    initial base service using generator v0.14.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/web/rest/UserResource.java
@@ -0,0 +1,46 @@
+package in.spotgarage.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import in.spotgarage.domain.User;
+import in.spotgarage.repository.UserRepository;
+import in.spotgarage.security.AuthoritiesConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import java.util.Optional;
+
+/**
+ * REST controller for managing users.
+ */
+@RestController
+@RequestMapping("/app")
+public class UserResource {
+
+    private final Logger log = LoggerFactory.getLogger(UserResource.class);
+
+    @Inject
+    private UserRepository userRepository;
+
+    /**
+     * GET  /rest/users/:login -> get the "login" user.
+     */
+    @RequestMapping(value = "/rest/users/{login}",
+            method = RequestMethod.GET,
+            produces = "application/json")
+    @Timed
+    @RolesAllowed(AuthoritiesConstants.ADMIN)
+    ResponseEntity<User> getUser(@PathVariable String login) {
+        log.debug("REST request to get User : {}", login);
+        return Optional.ofNullable(userRepository.findOne(login))
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
