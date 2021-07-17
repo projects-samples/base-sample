5e92931369c80d38635d30fc7cb60ecf3f414348
Pratik Raj <rajpratik71@gmail.com>
2021-07-18 00:47:11 +0530

    initial base service using generator v0.17.0
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/java/in/spotgarage/service/AuditEventService.java
@@ -0,0 +1,39 @@
+package in.spotgarage.service;
+
+import in.spotgarage.config.audit.AuditEventConverter;
+import in.spotgarage.domain.PersistentAuditEvent;
+import in.spotgarage.repository.PersistenceAuditEventRepository;
+import org.joda.time.LocalDateTime;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Service for managing audit events.
+ * <p/>
+ * <p>
+ * This is the default implementation to support SpringBoot Actuator AuditEventRepository
+ * </p>
+ */
+@Service
+public class AuditEventService {
+
+    @Inject
+    private PersistenceAuditEventRepository persistenceAuditEventRepository;
+
+    @Inject
+    private AuditEventConverter auditEventConverter;
+
+    public List<AuditEvent> findAll() {
+        return auditEventConverter.convertToAuditEvent(persistenceAuditEventRepository.findAll());
+    }
+
+    public List<AuditEvent> findByDates(LocalDateTime fromDate, LocalDateTime toDate) {
+        final List<PersistentAuditEvent> persistentAuditEvents =
+                persistenceAuditEventRepository.findByDates(fromDate, toDate);
+
+        return auditEventConverter.convertToAuditEvent(persistentAuditEvents);
+    }
+}
