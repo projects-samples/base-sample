cac14b9d25ed1e24fdce39b1c478a70780a343ad
Pratik Raj <rajpratik71@gmail.com>
2021-07-17 21:51:36 +0530

    initial base service using generator v4.0.3
    Signed-off-by: Pratik Raj <rajpratik71@gmail.com>

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,63 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    SpotGarageSharedLibsModule,
+    SpotGarageSharedCommonModule,
+    CSRFService,
+    AuthService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    JhiTrackerService,
+    HasAuthorityDirective,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        SpotGarageSharedLibsModule,
+        SpotGarageSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAuthorityDirective,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        SocialService,
+        AuthService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        SpotGarageSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAuthorityDirective,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class SpotGarageSharedModule {}
